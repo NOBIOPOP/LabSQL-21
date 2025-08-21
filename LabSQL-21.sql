58c5f1493febeb3f504d9d5c70d3dc823c5e785e
NOBI <ome123456cc@gmail.com>
2025-08-21 09:46:09 +0700

    Add files via upload

---
+++ LabSQL-21.sql
@@ -0,0 +1,39 @@
+﻿SELECT CategoryName, ProductName, UnitPrice
+FROM Categories , Products
+WHERE Categories.CategoryID = Products.CategoryID
+ORDER BY CategoryName
+
+SELECT CategoryName, ProductName, UnitPrice
+FROM Products join Categories
+on Products.CategoryID = Categories.CategoryID
+
+SELECT CategoryName, ProductName, UnitPrice
+FROM Products as p,Categories as c
+where p.CategoryID = c.CategoryID 
+
+SELECT CompanyName,OrderID
+from orders, shippers
+WHERE shippers.ShipperID = orders.ShipVia
+
+SELECT CompanyName,OrderID
+from orders JOIN shippers
+on shippers.ShipperID = orders.ShipVia
+
+SELECT ProductID, ProductName, CompanyName,Country 
+from Products p, Suppliers s
+WHERE p.SupplierID = s.SupplierID
+
+SELECT ProductID, ProductName, CompanyName,Country 
+from Products p JOIN Suppliers s
+on p.SupplierID = s.SupplierID
+
+SELECT CompanyName,OrderID
+FROM Orders ,Shippers
+where Shippers.ShipperID = Orders.ShipVia
+and OrderID = 10275
+
+SELECT ComponyName,OrderID
+FROM Orders JOIN Shippers
+WHERE shippers shipperID = Oeders.Shipvia
+AND CederID = 10275
+
