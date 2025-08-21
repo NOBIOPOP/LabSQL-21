fa800f4e9303c5fb319f49fecf518541e4a674d0
NOBI <ome123456cc@gmail.com>
2025-08-21 09:39:48 +0700

    Add files via upload

---
+++ instnwnd.sql
@@ -0,0 +1,20 @@
+﻿select c.catgryName,p.producname,p.unitpriice
+from products as p,Categories as c
+where p.CategoryID = C.CategoryID
+
+
+select catgryName,producname,unitpriice
+from products as p JOIN Categories as c
+where p.CategoryID = C.CategoryID
+
+select companyname,orderID
+from orders,Shippers
+where shippers,ShipperID = Orders.shipvia
+
+select companyname,orderID
+from orders,Shippers
+where shippers,ShipperID = Orders.shipvia
+
+SELECT ComponyName,OrderID
+FROM Orders Shippers
+WHERE shippers shipperID = Oeders.Shipvia
